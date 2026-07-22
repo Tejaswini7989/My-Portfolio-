0ffb90c1a6153c4a2aca5499e8e722aada8a237b
Tejaswini7989 <tejureddy7989@gmail.com>
2026-07-22 15:01:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link=>{
+link.addEventListener("click",function(e){
+e.preventDefault();
+
+const target=document.querySelector(this.getAttribute("href"));
+
+target.scrollIntoView({
+behavior:"smooth"
+});
+});
+});
+
+window.addEventListener("load",()=>{
+document.querySelector(".hero h1").style.opacity="1";
+});
